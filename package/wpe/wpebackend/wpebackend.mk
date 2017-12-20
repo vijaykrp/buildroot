ba4a7bf90a874c821c173615319e5f2dfe2b1226
Rene Barto <rene.barto66@gmail.com>
2017-12-20 23:43:39 +0100

    [WPEBackend] Bump to latest version again

--- package/wpe/wpebackend/wpebackend.mk
+++ package/wpe/wpebackend/wpebackend.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WPEBACKEND_VERSION = baa960b781a88addf778650e1226563ef12f8170
+WPEBACKEND_VERSION = db98fdd3442a1e008acfbd6ebe2ce4c8ae379208
 WPEBACKEND_SITE = $(call github,WebPlatformForEmbedded,WPEBackend,$(WPEBACKEND_VERSION))
 WPEBACKEND_INSTALL_STAGING = YES
 WPEBACKEND_DEPENDENCIES += libegl
