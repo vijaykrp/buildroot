381b47cfb4163ec4126abed604cd89d34ef3977b
Rene Barto <r.barto@metrological.com>
2018-01-08 17:41:24 +0100

    Updates to build with bumped version of wpeframework, as well as wpeframework-provisioning and libprovision.

--- board/synaptics/post-build.sh
+++ board/synaptics/post-build.sh
@@ -10,6 +10,8 @@ cp -pf "${BOARD_DIR}/index.html" "${TARGET_DIR}/www/"
 
 mkdir -p "${TARGET_DIR}/etc/init.d/"
 cp -pf "${BOARD_DIR}/S35wpasupplicant" "${TARGET_DIR}/etc/init.d/"
+cp -pf "${BOARD_DIR}/S41udhcpc-wlan0" "${TARGET_DIR}/etc/init.d/"
+cp -pf "${BOARD_DIR}/S42udhcpc-eth0" "${TARGET_DIR}/etc/init.d/"
 cp -pf "${BOARD_DIR}/S60ampservice" "${TARGET_DIR}/etc/init.d/"
 cp -pf "${BOARD_DIR}/S61keymap" "${TARGET_DIR}/etc/init.d/"
 cp -pf "${BOARD_DIR}/S62avsettings" "${TARGET_DIR}/etc/init.d/"
@@ -32,6 +34,10 @@ cp -pf "${BOARD_DIR}/90-remote-keymap.hwdb" "${TARGET_DIR}/etc/udev/hwdb.d"
 mkdir -p "${TARGET_DIR}/etc/udev/rules.d"
 cp -pf "${BOARD_DIR}/99-wpeframework-input-event.rules" "${TARGET_DIR}/etc/udev/rules.d"
 
+mkdir -p "${TARGET_DIR}/etc/wpa_supplicant"
+cp -pf "${BOARD_DIR}/wpa_supplicant.conf" "${TARGET_DIR}/etc/wpa_supplicant/wpa_supplicant-wlan0.conf"
+
+# Add LD_PRELOAD variable to WPEFramework script
 sed -i '/LD_PRELOAD/d' "${TARGET_DIR}/etc/init.d/S80WPEFramework"
 sed -i '/EGL_MVGFX/d' "${TARGET_DIR}/etc/init.d/S80WPEFramework"
 sed -i '/XDG_RUNTIME_DIR/a export LD_PRELOAD=libwesteros_gl.so.0.0.0' "${TARGET_DIR}/etc/init.d/S80WPEFramework"
